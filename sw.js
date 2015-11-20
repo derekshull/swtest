420126bdded83c0153ef63e4d44c212b228efc50
Matt Shull <derekshull@gmail.com>
2015-11-19 22:33:27 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-//Test
+// The files we want to cache
+var urlsToCache = [
+  '/swtest/',
+  '/swtest/styles.css',
+  '/swtest/app.js'
+];
+
+// Set the callback for the install step
+self.addEventListener('install', function(event) {
+    // Perform install steps
+});
