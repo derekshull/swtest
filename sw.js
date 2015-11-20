0d49d7b2f8437e0e1eafc7fdbe7f53a66f9486ed
Matt Shull <derekshull@gmail.com>
2015-11-19 22:41:01 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+var CACHE_NAME = 'my-site-cache-v1';
+
 // The files we want to cache
 var urlsToCache = [
   '/swtest/',
@@ -7,5 +9,12 @@ var urlsToCache = [
 
 // Set the callback for the install step
 self.addEventListener('install', function(event) {
-    // Perform install steps
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME) //Open cache
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache); //Cache the files into the cache.
+      })
+  );
 });
